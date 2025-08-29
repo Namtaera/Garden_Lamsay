1429bab396adad54155a456246700e2508aadbfd
namtaera <skaqhfk00@gmail.com>
2025-08-29 22:53:05 +0900

    hi_uu

---
+++ GardenLamsay/src/test/java/garden_lamsay/gardenlamsay/GardenLamsayApplicationTests.java
@@ -0,0 +1,13 @@
+package garden_lamsay.gardenlamsay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenLamsayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
