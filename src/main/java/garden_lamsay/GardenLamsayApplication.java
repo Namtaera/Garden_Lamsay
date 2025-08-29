56ce12333b7c5af2bc2808dd0d295346df29e514
namtaera <skaqhfk00@gmail.com>
2025-08-30 02:14:53 +0900

    hi

--- src/main/java/garden_lamsay/GardenLamsayApplication.java
+++ src/main/java/garden_lamsay/GardenLamsayApplication.java
@@ -3,7 +3,7 @@ package garden_lamsay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "garden_lamsay")
+@SpringBootApplication
 public class GardenLamsayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GardenLamsayApplication.class, args);
